6dba67febde647b42fe322e34363ed50695ece14
kednar <kednar@gmail.com>
2017-04-25 11:17:47 +0200

    updated character animations and sprites

--- characters/space/space_anims.gd
+++ characters/space/space_anims.gd
@@ -1,9 +1,5 @@
 # angle "0" is facing the camera, rotates counter clock-wise, angle 90 is profile right, etc
-
-const dir_angles = [180, 360]
-
-const directions = ["walk_left", 1, "walk_left", -1]
-
-const idles = ["idle_left", 1, "idle_left", -1]
-
-const speaks = ["idle_left", 1, "idle_left", -1]
+const dir_angles = [ 180, 360 ]
+const directions = [ "walk_right", 1, "walk_right", -1 ]
+const idles = [ "idle_right", 1, "idle_right", -1 ]
+const speaks = [ "pick_up", 1, "pick_up", -1 ]
---
+++ rooms/space/hotspot_cantmove.gd
@@ -0,0 +1,11 @@
+#extends CollisionPolygon2D
+
+#
+#signal cantmove
+
+#func _input(event):
+#	if event.type == InputEvent.MOUSE_BUTTON && event.pressed:
+#		emit_signal("cantmove")
+#
+#func _ready():
+#	set_process_input(true)
