a324745a410985102dcaae35d75e681ee3dea4c6
kednar <kednar@gmail.com>
2017-05-01 00:56:54 +0200

    implemented flesk script to replace character when pipe is taken

--- rooms/space/cutscene_pipe.gd
+++ rooms/space/cutscene_pipe.gd
@@ -1,20 +1,28 @@
 extends Node2D
 
 var seconds = 0
-var cutscene_pipe_2
+var space_scene
+var has_dialogue = true
+
+func skip_dialogue():
+	if has_dialogue:
+		get_node("text").set_bbcode("")
+		has_dialogue = false
+	else:
+		get_tree().change_scene_to(space_scene)
 	
 func _input(event):
 	if event.type == InputEvent.MOUSE_BUTTON && event.pressed:
-		get_tree().change_scene_to(cutscene_pipe_2)
+		skip_dialogue()
 
 func _ready():
 	# Preload main scene
-	cutscene_pipe_2 = preload("res://rooms/space/cutscene_pipe_2.tscn")
+	space_scene = preload("res://rooms/space/space_1.tscn")
 	set_process_input(true)
 	set_process(true)
 
 func _process(delta):
     # Add up seconds passed
 	seconds += delta
-	if (seconds >= 5) || seconds >= 10:
-		get_tree().change_scene_to(cutscene_pipe_2)
+	if (seconds >= 5 && has_dialogue) || seconds >= 10:
+		skip_dialogue()
